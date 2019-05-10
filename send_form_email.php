c5af4990124a78b7cd82d8a8b913f11af7a4db58
Haci Karahasanoglu <haci@hacikho.com>
2019-05-10 09:11:21 -0400

    email change

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "hkarahasanoglu@firstenergycorp.com";
+    $email_to = "hacikho01@gmail.com";
     $email_subject = "Your email subject line";
  
     function died($error) {
